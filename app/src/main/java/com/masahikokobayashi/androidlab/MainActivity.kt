c229019c23d532b337c0c7bf6328cfa8b89d67a6
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-06-12 08:04:08 +0900

    feat: greeting

--- app/src/main/java/com/masahikokobayashi/androidlab/MainActivity.kt
+++ app/src/main/java/com/masahikokobayashi/androidlab/MainActivity.kt
@@ -2,9 +2,13 @@ package com.masahikokobayashi.androidlab
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.compose.runtime.Composable
 
 class MainActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 }
+
+@Composable
+fun Greeting() {}
