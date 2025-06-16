fc0e0b1796e7152504ac464961e81af4b818f08c
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-06-17 07:31:37 +0900

    feat: set content

--- app/src/main/java/com/masahikokobayashi/androidlab/MainActivity.kt
+++ app/src/main/java/com/masahikokobayashi/androidlab/MainActivity.kt
@@ -2,6 +2,7 @@ package com.masahikokobayashi.androidlab
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -14,6 +15,9 @@ import androidx.compose.ui.unit.dp
 class MainActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+            Greeting("Android")
+        }
     }
 }
 
