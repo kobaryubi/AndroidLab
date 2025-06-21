d8c0aa6d5fa6a1710db6bb8910eadee18c1254a1
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-06-22 08:38:50 +0900

    feat: theme

---
+++ app/src/main/java/com/masahikokobayashi/androidlab/ui/theme/Theme.kt
@@ -0,0 +1,13 @@
+package com.masahikokobayashi.androidlab.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AndroidLabTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        content = content
+    )
+}
