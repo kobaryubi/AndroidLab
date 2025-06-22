13d682609b7cdbc0087280bb50cf806ba4d7acac
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-06-22 10:52:35 +0900

    feat: extract string

--- app/src/main/java/com/masahikokobayashi/androidlab/MainActivity.kt
+++ app/src/main/java/com/masahikokobayashi/androidlab/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,8 +32,8 @@ class MainActivity: ComponentActivity() {
 
             AndroidLabTheme {
                 GreetingImage(
-                    message = "Happy Birthday Sam!",
-                    from = "From Emma",
+                    message = stringResource(R.string.happy_birthday_text),
+                    from = stringResource(R.string.signature_text),
                 )
             }
         }
@@ -87,8 +88,8 @@ fun GreetingPreview() {
 fun AndroidLabPreview() {
     AndroidLabTheme {
         GreetingImage(
-            message = "Happy Birthday Sam!",
-            from = "From Emma",
+            message = stringResource(R.string.happy_birthday_text),
+            from = stringResource(R.string.signature_text),
         )
     }
 }
