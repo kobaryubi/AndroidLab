cf960600767eaf285dac99ae395b943173d6ba1c
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-06-25 07:57:18 +0900

    feat: dice roll

--- app/src/main/java/com/masahikokobayashi/androidlab/MainActivity.kt
+++ app/src/main/java/com/masahikokobayashi/androidlab/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
@@ -60,6 +62,7 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
             painter = painterResource(R.drawable.dice_1),
             contentDescription = "1"
         )
+        Spacer(modifier = Modifier.height(16.dp))
         Button(onClick = { /*TODO*/ }) {
             Text(stringResource(R.string.roll))
         }
