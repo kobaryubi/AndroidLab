0c3245a61e445a4c48dd3c2e0411662bbc9789e6
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-06-28 23:04:50 +0900

    feat: theme

--- app/src/main/java/com/masahikokobayashi/androidlab/ui/theme/ReplyTheme.kt
+++ app/src/main/java/com/masahikokobayashi/androidlab/ui/theme/ReplyTheme.kt
@@ -1,13 +1,27 @@
 package com.masahikokobayashi.androidlab.ui.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+
+private val LightColorScheme = lightColorScheme()
+private val DarkColorSchema = darkColorScheme()
 
 @Composable
 fun ReplyTheme(
+    useDarkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
+    val colorScheme = if (useDarkTheme) {
+        DarkColorSchema
+    } else {
+        LightColorScheme
+    }
+
     MaterialTheme(
+        colorScheme = colorScheme,
         content = content
     )
 }
