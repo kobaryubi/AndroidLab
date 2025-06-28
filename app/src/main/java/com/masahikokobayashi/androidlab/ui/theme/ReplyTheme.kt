f232d8320b1b928af7a9fab076ed87f4bd7e3cfd
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-06-28 22:52:32 +0900

    feat: theme

---
+++ app/src/main/java/com/masahikokobayashi/androidlab/ui/theme/ReplyTheme.kt
@@ -0,0 +1,13 @@
+package com.masahikokobayashi.androidlab.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ReplyTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        content = content
+    )
+}
