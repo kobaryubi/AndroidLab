25c12b4aee5265d4318b8f0939a4b8360521ede5
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-06-29 08:49:22 +0900

    feat: lemon

---
+++ app/src/main/java/com/masahikokobayashi/androidlab/LemonTextAndImage.kt
@@ -0,0 +1,41 @@
+package com.masahikokobayashi.androidlab
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LemonTextAndImage(
+    textLabelResourceId: Int,
+    drawableResourceId: Int,
+    contentDescriptionResourceId: Int,
+    onImageClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+            .fillMaxSize()
+    ) {
+        Button(onClick = onImageClick) {
+            Image(
+                painter = painterResource(drawableResourceId),
+                contentDescription = stringResource(contentDescriptionResourceId)
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(stringResource(textLabelResourceId))
+    }
+}
