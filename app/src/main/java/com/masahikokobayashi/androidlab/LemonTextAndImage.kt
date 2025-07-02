ecb1b928a1d794e7f973bfdb18957c3c6b07fcad
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-02 16:02:43 +0900

    feat: dimens

--- app/src/main/java/com/masahikokobayashi/androidlab/LemonTextAndImage.kt
+++ app/src/main/java/com/masahikokobayashi/androidlab/LemonTextAndImage.kt
@@ -6,14 +6,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
@@ -30,13 +33,17 @@ fun LemonTextAndImage(
         modifier = modifier
             .fillMaxSize()
     ) {
-        Button(onClick = onImageClick) {
+        Button(
+            onClick = onImageClick,
+            shape = RoundedCornerShape(dimensionResource(R.dimen.button_corner_radius)),
+            colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.tertiaryContainer)
+        ) {
             Image(
                 painter = painterResource(drawableResourceId),
-                contentDescription = stringResource(contentDescriptionResourceId)
+                contentDescription = stringResource(contentDescriptionResourceId),
             )
         }
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(dimensionResource(R.dimen.padding_vertical)))
         Text(
             text = stringResource(textLabelResourceId),
             fontSize = 18.sp
