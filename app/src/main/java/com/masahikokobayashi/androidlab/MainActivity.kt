9e7d9fc9cedb9e157f5efc0881db6cc2989ed9b5
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-04 07:59:00 +0900

    feat: padding

--- app/src/main/java/com/masahikokobayashi/androidlab/MainActivity.kt
+++ app/src/main/java/com/masahikokobayashi/androidlab/MainActivity.kt
@@ -56,6 +56,7 @@ class MainActivity: ComponentActivity() {
                     TipTimeLayout(
                         modifier = Modifier
                             .statusBarsPadding()
+                            .padding(horizontal = 40.dp)
                     )
                 }
             }
--- app/src/main/java/com/masahikokobayashi/androidlab/TipTimeLayout.kt
+++ app/src/main/java/com/masahikokobayashi/androidlab/TipTimeLayout.kt
@@ -1,11 +1,15 @@
 package com.masahikokobayashi.androidlab
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.masahikokobayashi.androidlab.ui.theme.TipTimeTheme
 
 @Composable
@@ -25,6 +29,13 @@ fun TipTimeLayout(
 @Composable
 fun TipTimeLayoutPreview() {
     TipTimeTheme {
-        TipTimeLayout()
+        Surface(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            TipTimeLayout(
+                modifier = Modifier
+                    .padding(horizontal = 40.dp)
+            )
+        }
     }
 }
