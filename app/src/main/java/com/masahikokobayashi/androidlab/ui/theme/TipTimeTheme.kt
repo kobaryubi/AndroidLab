72b67f3a1e87aa6e435e47b4e9b950530391a083
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-04 07:26:01 +0900

    feat: theme

---
+++ app/src/main/java/com/masahikokobayashi/androidlab/ui/theme/TipTimeTheme.kt
@@ -0,0 +1,13 @@
+package com.masahikokobayashi.androidlab.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TipTimeTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        content = content
+    )
+}
