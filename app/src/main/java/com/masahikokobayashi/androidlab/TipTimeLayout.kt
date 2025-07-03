2e5ea4ccf483c02242ea7910311c2b18f0a68067
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-04 07:32:14 +0900

    feat: layout

---
+++ app/src/main/java/com/masahikokobayashi/androidlab/TipTimeLayout.kt
@@ -0,0 +1,21 @@
+package com.masahikokobayashi.androidlab
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.masahikokobayashi.androidlab.ui.theme.TipTimeTheme
+
+@Composable
+fun TipTimeLayout(
+    modifier: Modifier = Modifier
+) {
+
+}
+
+@Preview
+@Composable
+fun TipTimeLayoutPreview() {
+    TipTimeTheme {
+        TipTimeLayout()
+    }
+}
