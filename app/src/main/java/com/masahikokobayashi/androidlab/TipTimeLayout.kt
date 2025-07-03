d438cc5502c521e9c4301b0c70c8e0ab1b1fd576
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-04 07:37:59 +0900

    feat: layout

--- app/src/main/java/com/masahikokobayashi/androidlab/TipTimeLayout.kt
+++ app/src/main/java/com/masahikokobayashi/androidlab/TipTimeLayout.kt
@@ -1,7 +1,10 @@
 package com.masahikokobayashi.androidlab
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.masahikokobayashi.androidlab.ui.theme.TipTimeTheme
 
@@ -9,7 +12,13 @@ import com.masahikokobayashi.androidlab.ui.theme.TipTimeTheme
 fun TipTimeLayout(
     modifier: Modifier = Modifier
 ) {
-
+    Column(
+        modifier = modifier
+    ) {
+        Text(
+            text = stringResource(R.string.calculate_tip)
+        )
+    }
 }
 
 @Preview
