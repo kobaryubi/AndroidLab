9d1b1a5cf9afab94f151bb309b1141aed526e052
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-04 14:36:02 +0900

    feat: align

--- app/src/main/java/com/masahikokobayashi/androidlab/TipTimeLayout.kt
+++ app/src/main/java/com/masahikokobayashi/androidlab/TipTimeLayout.kt
@@ -1,11 +1,13 @@
 package com.masahikokobayashi.androidlab
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -17,7 +19,9 @@ fun TipTimeLayout(
     modifier: Modifier = Modifier
 ) {
     Column(
-        modifier = modifier
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
         Text(
             text = stringResource(R.string.calculate_tip)
