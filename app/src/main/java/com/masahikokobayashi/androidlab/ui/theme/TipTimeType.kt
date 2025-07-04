af7f0e85e3b70eab104f9731a9fb468eb19656ba
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-04 22:04:03 +0900

    feat: typography

---
+++ app/src/main/java/com/masahikokobayashi/androidlab/ui/theme/TipTimeType.kt
@@ -0,0 +1,13 @@
+package com.masahikokobayashi.androidlab.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+
+val Typography = Typography(
+    displaySmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+    )
+)
