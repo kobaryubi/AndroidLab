7e05ac6730e4310a55cfc6a9f598c62df8602bab
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-05 21:40:13 +0900

    feat: roundup

--- app/src/main/java/com/masahikokobayashi/androidlab/TipTimeLayout.kt
+++ app/src/main/java/com/masahikokobayashi/androidlab/TipTimeLayout.kt
@@ -35,8 +35,12 @@ import java.text.NumberFormat
  * according to the local currency.
  * Example would be "$10.00".
  */
-private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
-    val tip = tipPercent / 100 * amount
+private fun calculateTip(amount: Double, tipPercent: Double = 15.0, roundUp: Boolean): String {
+    var tip = tipPercent / 100 * amount
+    if (roundUp) {
+        tip = kotlin.math.ceil(tip)
+    }
+
     return NumberFormat.getCurrencyInstance().format(tip)
 }
 
@@ -50,9 +54,9 @@ fun TipTimeLayout(
     var tipInput by remember { mutableStateOf("")}
     val tipPercent = tipInput.toDoubleOrNull() ?: 0.0
 
-    val tip = calculateTip(amount, tipPercent)
-    
     var roundUp by remember { mutableStateOf(false) }
+
+    val tip = calculateTip(amount, tipPercent, roundUp)
 
     Column(
         modifier = modifier,
