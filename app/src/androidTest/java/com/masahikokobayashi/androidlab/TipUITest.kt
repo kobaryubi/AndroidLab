d4df2352a1e14200515ed0e764bb95d5d8ab7298
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-20 06:32:21 +0900

    test: runner

--- app/src/androidTest/java/com/masahikokobayashi/androidlab/TipUITest.kt
+++ app/src/androidTest/java/com/masahikokobayashi/androidlab/TipUITest.kt
@@ -2,9 +2,11 @@ package com.masahikokobayashi.androidlab
 
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextInput
 import com.masahikokobayashi.androidlab.ui.theme.TipTimeTheme
 import org.junit.Rule
 import org.junit.Test
+import java.text.NumberFormat
 
 class TipUITest {
     @get:Rule
@@ -18,5 +20,11 @@ class TipUITest {
             }
         }
         composeTestRule.onNodeWithText("Bill Amount")
+            .performTextInput("10")
+        composeTestRule.onNodeWithText("Tip Percentage").performTextInput("20")
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        composeTestRule.onNodeWithText("Tip Amount: $expectedTip").assertExists(
+            "No node with this text was found."
+        )
     }
 }
