5572860e664caeea2a2adb3040899f2aeba9da7b
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-20 08:07:43 +0900

    feat: preview

--- app/src/main/java/com/masahikokobayashi/androidlab/AffirmationCard.kt
+++ app/src/main/java/com/masahikokobayashi/androidlab/AffirmationCard.kt
@@ -1,8 +1,34 @@
 package com.masahikokobayashi.androidlab
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import com.masahikokobayashi.androidlab.model.Affirmation
 
 @Composable
-fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {}
+fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
+    Card(modifier = modifier) {
+        Column {
+            Image(
+                painter = painterResource(affirmation.imageResourceId),
+                contentDescription = stringResource(affirmation.stringResourceId)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun AffirmationCardPreview() {
+    AffirmationCard(
+        Affirmation(
+            stringResourceId = R.string.affirmation1,
+            imageResourceId = R.drawable.image1
+        )
+    )
+}
