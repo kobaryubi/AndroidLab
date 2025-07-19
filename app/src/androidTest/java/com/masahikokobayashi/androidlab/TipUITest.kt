247de1abba36d51e17a2d1cdf162857c55918166
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-20 06:26:26 +0900

    test: runner

--- app/src/androidTest/java/com/masahikokobayashi/androidlab/TipUITest.kt
+++ app/src/androidTest/java/com/masahikokobayashi/androidlab/TipUITest.kt
@@ -1,7 +1,22 @@
 package com.masahikokobayashi.androidlab
 
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import com.masahikokobayashi.androidlab.ui.theme.TipTimeTheme
+import org.junit.Rule
+import org.junit.Test
 
 class TipUITest {
+    @get:Rule
     val composeTestRule = createComposeRule()
+
+    @Test
+    fun calculate_20_percent_tip() {
+        composeTestRule.setContent {
+            TipTimeTheme {
+                TipTimeLayout()
+            }
+        }
+        composeTestRule.onNodeWithText("Bill Amount")
+    }
 }
