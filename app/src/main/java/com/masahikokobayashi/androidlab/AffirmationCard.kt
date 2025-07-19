eff5a1686e69b1e6788ceb03de4a4f70d43add8f
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-20 08:12:09 +0900

    feat: markup

--- app/src/main/java/com/masahikokobayashi/androidlab/AffirmationCard.kt
+++ app/src/main/java/com/masahikokobayashi/androidlab/AffirmationCard.kt
@@ -2,12 +2,19 @@ package com.masahikokobayashi.androidlab
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.masahikokobayashi.androidlab.model.Affirmation
 
 @Composable
@@ -16,7 +23,16 @@ fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
         Column {
             Image(
                 painter = painterResource(affirmation.imageResourceId),
-                contentDescription = stringResource(affirmation.stringResourceId)
+                contentDescription = stringResource(affirmation.stringResourceId),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(194.dp),
+                contentScale = ContentScale.Crop
+            )
+            Text(
+                text = stringResource(affirmation.stringResourceId),
+                modifier = Modifier.padding(16.dp),
+                style = MaterialTheme.typography.headlineSmall
             )
         }
     }
