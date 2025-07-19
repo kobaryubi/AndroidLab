3c7c5269b4a038781d43305104ac4e14a66450ae
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-19 18:52:46 +0900

    test: tip

--- app/src/test/java/com/masahikokobayashi/androidlab/TipCalculatorTest.kt
+++ app/src/test/java/com/masahikokobayashi/androidlab/TipCalculatorTest.kt
@@ -1,8 +1,20 @@
 package com.masahikokobayashi.androidlab
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
+import java.text.NumberFormat
 
 class TipCalculatorTest {
     @Test
-    fun calculateTip_20PercentNoRoundup() {}
+    fun calculateTip_20PercentNoRoundup() {
+        val amount = 10.0
+        val tipPercent = 20.0
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        val actualTip = calculateTip(
+            amount = amount,
+            tipPercent = tipPercent,
+            roundUp = false
+        )
+        assertEquals(expectedTip, actualTip)
+    }
 }
