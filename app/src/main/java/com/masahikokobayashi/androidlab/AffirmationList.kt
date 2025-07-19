9449b525b991512bcf463ce758e6232ee6acdf83
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-20 08:15:56 +0900

    feat: markup

---
+++ app/src/main/java/com/masahikokobayashi/androidlab/AffirmationList.kt
@@ -0,0 +1,23 @@
+package com.masahikokobayashi.androidlab
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.masahikokobayashi.androidlab.model.Affirmation
+import androidx.compose.foundation.lazy.items
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AffirmationList(
+    affirmationList: List<Affirmation>
+    , modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(affirmationList) { affirmation ->
+            AffirmationCard(
+                affirmation = affirmation,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
+}
