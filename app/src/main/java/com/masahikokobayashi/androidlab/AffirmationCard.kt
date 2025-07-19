2da4d0209db41f716b1962b33811f29862b8f260
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-20 07:25:41 +0900

    feat: affirmation

---
+++ app/src/main/java/com/masahikokobayashi/androidlab/AffirmationCard.kt
@@ -0,0 +1,8 @@
+package com.masahikokobayashi.androidlab
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.masahikokobayashi.androidlab.model.Affirmation
+
+@Composable
+fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {}
---
+++ app/src/main/java/com/masahikokobayashi/androidlab/AffirmationsApp.kt
@@ -0,0 +1,7 @@
+package com.masahikokobayashi.androidlab
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AffirmationsApp() {
+}
