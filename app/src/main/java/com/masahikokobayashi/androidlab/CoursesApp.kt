b8abd5c3b9008a6a4cfd61f8de249b8e08b8e6d7
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-20 19:11:03 +0900

    feat: android

--- app/src/main/java/com/masahikokobayashi/androidlab/CoursesApp.kt
+++ app/src/main/java/com/masahikokobayashi/androidlab/CoursesApp.kt
@@ -1,10 +1,18 @@
 package com.masahikokobayashi.androidlab
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import com.masahikokobayashi.androidlab.data.Datasource
 
 @Composable
 fun CoursesApp(modifier: Modifier = Modifier) {
-    TopicGrid(topics = Datasource.topics, modifier = modifier)
+    TopicGrid(
+        topics = Datasource.topics, modifier = modifier.padding(
+            start = dimensionResource(R.dimen.padding_small),
+            top = dimensionResource(R.dimen.padding_small),
+            end = dimensionResource(R.dimen.padding_small),
+        )
+    )
 }
