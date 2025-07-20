4831e107033ceaed861329605330515d03527a79
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-20 10:44:43 +0900

    feat: android

--- app/src/main/java/com/masahikokobayashi/androidlab/TopicCard.kt
+++ app/src/main/java/com/masahikokobayashi/androidlab/TopicCard.kt
@@ -3,13 +3,17 @@ package com.masahikokobayashi.androidlab
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.masahikokobayashi.androidlab.data.Datasource
 import com.masahikokobayashi.androidlab.model.Topic
 
@@ -22,7 +26,11 @@ fun TopicCard(
         Row {
             Image(
                 painter = painterResource(topic.imageResourceId),
-                contentDescription = stringResource(topic.stringResourceId)
+                contentDescription = stringResource(topic.stringResourceId),
+                modifier = Modifier
+                    .width(68.dp)
+                    .height(68.dp),
+                contentScale = ContentScale.Crop
             )
             Column {
                 Text(
--- app/src/main/java/com/masahikokobayashi/androidlab/TopicGrid.kt
+++ app/src/main/java/com/masahikokobayashi/androidlab/TopicGrid.kt
@@ -6,7 +6,9 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.masahikokobayashi.androidlab.data.Datasource
 import com.masahikokobayashi.androidlab.model.Topic
 
 @Composable
@@ -24,4 +26,12 @@ fun TopicGrid(
             TopicCard(topic)
         }
     }
+}
+
+@Preview
+@Composable
+private fun TopicGridPreview() {
+    TopicGrid(
+        topics = Datasource.topics,
+    )
 }
