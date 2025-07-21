6af59840a8c859cf4880bec70d73eaa8d09551f4
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-21 11:32:32 +0900

    feat: android

--- app/src/main/java/com/masahikokobayashi/androidlab/data/Datasource.kt
+++ app/src/main/java/com/masahikokobayashi/androidlab/data/Datasource.kt
@@ -2,6 +2,7 @@ package com.masahikokobayashi.androidlab.data
 
 import com.masahikokobayashi.androidlab.R
 import com.masahikokobayashi.androidlab.model.Affirmation
+import com.masahikokobayashi.androidlab.model.Dog
 import com.masahikokobayashi.androidlab.model.Topic
 
 object Datasource {
@@ -36,4 +37,16 @@ object Datasource {
             Affirmation(R.string.affirmation10, R.drawable.image10)
         )
     }
+
+    val dogs = listOf(
+         Dog(R.drawable.koda, R.string.dog_name_1, 2, R.string.dog_description_1),
+         Dog(R.drawable.lola, R.string.dog_name_2, 16, R.string.dog_description_2),
+         Dog(R.drawable.frankie, R.string.dog_name_3, 2, R.string.dog_description_3),
+         Dog(R.drawable.nox, R.string.dog_name_4, 8, R.string.dog_description_4),
+         Dog(R.drawable.faye, R.string.dog_name_5, 8, R.string.dog_description_5),
+         Dog(R.drawable.bella, R.string.dog_name_6, 14, R.string.dog_description_6),
+         Dog(R.drawable.moana, R.string.dog_name_7, 2, R.string.dog_description_7),
+         Dog(R.drawable.tzeitel, R.string.dog_name_8, 7, R.string.dog_description_8),
+         Dog(R.drawable.leroy, R.string.dog_name_9, 4, R.string.dog_description_9)
+    )
 }
