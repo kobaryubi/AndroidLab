fe7850255fa9ef07683b2b864d704386ae4f0e50
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-21 18:51:25 +0900

    feat: android

---
+++ app/src/main/java/com/masahikokobayashi/androidlab/DogHobby.kt
@@ -0,0 +1,25 @@
+package com.masahikokobayashi.androidlab
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+
+@Composable
+fun DogHobby(
+    @StringRes dogHobby: Int, modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Text(
+            text = stringResource(R.string.about), style = MaterialTheme.typography.labelSmall
+        )
+        Text(
+            text = stringResource(dogHobby), style = MaterialTheme.typography.bodyLarge
+        )
+    }
+}
