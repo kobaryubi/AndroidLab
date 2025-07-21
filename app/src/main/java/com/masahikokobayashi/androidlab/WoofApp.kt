510afc38f4cb4261bd29d25143c572cff72e2415
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-21 12:53:11 +0900

    feat: android

--- app/src/main/java/com/masahikokobayashi/androidlab/WoofApp.kt
+++ app/src/main/java/com/masahikokobayashi/androidlab/WoofApp.kt
@@ -1,12 +1,29 @@
 package com.masahikokobayashi.androidlab
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.masahikokobayashi.androidlab.data.Datasource
 import com.masahikokobayashi.androidlab.ui.theme.woof.WoofTheme
+import androidx.compose.foundation.lazy.items
+import androidx.compose.ui.res.dimensionResource
 
 @Composable
 fun WoofApp(modifier: Modifier = Modifier) {
+    Scaffold(modifier = modifier) { innerPadding ->
+        LazyColumn(contentPadding = innerPadding) {
+            items(Datasource.dogs) { dog ->
+                DogItem(
+                    dog = dog, modifier = Modifier.padding(
+                        dimensionResource(R.dimen.padding_small)
+                    )
+                )
+            }
+        }
+    }
 }
 
 @Preview
