954e107b61cb1b4dd904c01807e5946b24d45881
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-21 12:25:08 +0900

    feat: android

---
+++ app/src/main/java/com/masahikokobayashi/androidlab/model/Dog.kt
@@ -0,0 +1,11 @@
+package com.masahikokobayashi.androidlab.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Dog(
+    @DrawableRes val imageResourceId: Int,
+    @StringRes val name: Int,
+    val age: Int,
+    @StringRes val hobbies: Int
+)
