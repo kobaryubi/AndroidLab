3fc13b438bdb7aa59bf93cafc78063d9041a75b5
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-21 11:16:46 +0900

    feat: android

--- app/src/main/java/com/masahikokobayashi/androidlab/WoofApp.kt
+++ app/src/main/java/com/masahikokobayashi/androidlab/WoofApp.kt
@@ -2,7 +2,25 @@ package com.masahikokobayashi.androidlab
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.masahikokobayashi.androidlab.ui.theme.woof.WoofTheme
 
 @Composable
 fun WoofApp(modifier: Modifier = Modifier) {
+}
+
+@Preview
+@Composable
+private fun WoofAppPreview() {
+    WoofTheme {
+        WoofApp()
+    }
+}
+
+@Preview
+@Composable
+private fun WoofAppDarkThemePreview() {
+    WoofTheme(darkTheme = true) {
+        WoofApp()
+    }
 }
