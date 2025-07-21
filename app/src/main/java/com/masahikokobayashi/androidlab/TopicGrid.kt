f675e3b1f0e1fdee3033a62a4fa38715474e0c51
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-21 12:59:27 +0900

    feat: android

--- app/src/main/java/com/masahikokobayashi/androidlab/TopicGrid.kt
+++ app/src/main/java/com/masahikokobayashi/androidlab/TopicGrid.kt
@@ -8,7 +8,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.masahikokobayashi.androidlab.data.Datasource
 import com.masahikokobayashi.androidlab.model.Topic
 
