8645282cd88a50a9cf668457ed58a9ae841744b7
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-21 17:03:27 +0900

    feat: android

--- app/src/main/java/com/masahikokobayashi/androidlab/DogItem.kt
+++ app/src/main/java/com/masahikokobayashi/androidlab/DogItem.kt
@@ -1,19 +1,34 @@
 package com.masahikokobayashi.androidlab
 
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import com.masahikokobayashi.androidlab.model.Dog
 
 @Composable
-fun DogIItem(
+fun DogItem(
     dog: Dog,
     modifier: Modifier = Modifier,
 ) {
     Card(
         modifier = modifier
     ) {
-        Row {}
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(R.dimen.padding_small))
+        ) {
+            DogIcon(
+                dogIcon = dog.imageResourceId,
+            )
+            DogInformation(
+                dogName = dog.name,
+                dogAge = dog.age,
+            )
+        }
     }
 }
