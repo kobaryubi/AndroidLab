5fb68e19aaef15174e90048f53213013b252bdbd
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-21 13:20:20 +0900

    feat: android

---
+++ app/src/main/java/com/masahikokobayashi/androidlab/DogItemButton.kt
@@ -0,0 +1,28 @@
+package com.masahikokobayashi.androidlab
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+
+@Composable
+fun DogItemButton(
+    expanded: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    IconButton(
+        onClick = onClick,
+        modifier = modifier
+    ) {
+        Icon(
+            imageVector = Icons.Filled.ExpandMore,
+            contentDescription = stringResource(R.string.expand_button_content_description),
+            tint = MaterialTheme.colorScheme.secondary
+        )
+    }
+}
