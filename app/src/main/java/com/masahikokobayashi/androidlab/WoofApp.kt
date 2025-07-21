a354c8fc8e655df79bd44cfbed4c0c7258c71110
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-21 11:08:49 +0900

    feat: android

---
+++ app/src/main/java/com/masahikokobayashi/androidlab/WoofApp.kt
@@ -0,0 +1,8 @@
+package com.masahikokobayashi.androidlab
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun WoofApp(modifier: Modifier = Modifier) {
+}
