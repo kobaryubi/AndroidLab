6c4bada13a050e21121e2437a90343181193745c
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-21 15:16:25 +0900

    feat: android

--- app/src/main/java/com/masahikokobayashi/androidlab/WoofTopAppBar.kt
+++ app/src/main/java/com/masahikokobayashi/androidlab/WoofTopAppBar.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
@@ -19,7 +20,9 @@ import androidx.compose.ui.res.stringResource
 fun WoofTopAppBar(modifier: Modifier = Modifier) {
     CenterAlignedTopAppBar(
         title = {
-            Row {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
                 Image(
                     painter = painterResource(R.drawable.ic_woof_logo),
                     contentDescription = null,
@@ -33,6 +36,6 @@ fun WoofTopAppBar(modifier: Modifier = Modifier) {
                 )
             }
         },
-        modifier = modifier
+        modifier = modifier,
     )
 }
