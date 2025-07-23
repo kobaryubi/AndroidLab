f3f7f04df57642bcb3fbb1b49422d09204e37905
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-23 19:00:59 +0900

    feat: android

---
+++ app/src/main/java/com/masahikokobayashi/androidlab/ui/GameScreen.kt
@@ -0,0 +1,16 @@
+package com.masahikokobayashi.androidlab.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.masahikokobayashi.androidlab.ui.theme.UnscrambleTheme
+
+@Composable
+fun GameScreen() {}
+
+@Preview
+@Composable
+private fun GameScreenPreview() {
+    UnscrambleTheme {
+        GameScreen()
+    }
+}
