f9d7cd32cc23fb8955143b4d59a8de721d081430
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-24 05:58:43 +0900

    feat: android

--- app/src/main/java/com/masahikokobayashi/androidlab/ui/GameScreen.kt
+++ app/src/main/java/com/masahikokobayashi/androidlab/ui/GameScreen.kt
@@ -1,11 +1,33 @@
 package com.masahikokobayashi.androidlab.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.MaterialTheme.typography
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.masahikokobayashi.androidlab.R
 import com.masahikokobayashi.androidlab.ui.theme.UnscrambleTheme
 
 @Composable
-fun GameScreen() {}
+fun GameScreen(
+    modifier: Modifier = Modifier,
+    gameViewModel: GameViewModel = viewModel(),
+) {
+    val gameUiState by gameViewModel.uiState.collectAsState()
+
+    Column(modifier = modifier) {
+        Text(
+            text = stringResource(R.string.app_name),
+            style = typography.titleLarge
+        )
+        GameLayout()
+    }
+}
 
 @Preview
 @Composable
