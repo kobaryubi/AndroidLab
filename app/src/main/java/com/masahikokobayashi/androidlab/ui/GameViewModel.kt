c1ae6b15b787d18752385808da21b32bee08098b
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-23 12:19:06 +0900

    feat: android

--- app/src/main/java/com/masahikokobayashi/androidlab/ui/GameViewModel.kt
+++ app/src/main/java/com/masahikokobayashi/androidlab/ui/GameViewModel.kt
@@ -1,5 +1,8 @@
 package com.masahikokobayashi.androidlab.ui
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 
-class GameViewModel : ViewModel() {}
+class GameViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(GameUiState())
+}
