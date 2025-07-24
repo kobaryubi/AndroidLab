5a99acebe81060c1c84e7eb87507853123561a38
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-24 16:51:02 +0900

    feat: android

--- app/src/main/java/com/masahikokobayashi/androidlab/MainActivity.kt
+++ app/src/main/java/com/masahikokobayashi/androidlab/MainActivity.kt
@@ -52,6 +52,31 @@ class MainActivity : ComponentActivity() {
         Log.d(TAG, "onStart Called")
     }
 
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume Called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart Called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause Called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop Called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy Called")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "onCreate Called")
