fd6701508ad937a96ff24ec592fcfe8424e40068
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-25 06:04:13 +0900

    feat: android

---
+++ app/src/main/java/com/masahikokobayashi/androidlab/ui/TransactionInfo.kt
@@ -0,0 +1,29 @@
+package com.masahikokobayashi.androidlab.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import com.masahikokobayashi.androidlab.R
+
+@Composable
+fun TransactionInfo(
+    revenue: Int,
+    dessertsSold: Int,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier) {
+        DessertsSoldInfo(
+            dessertsSold = dessertsSold,
+            modifier = Modifier.fillMaxWidth()
+                .padding(dimensionResource(R.dimen.padding_medium))
+        )
+        RevenueInfo(
+            revenue = revenue,
+            modifier = Modifier.fillMaxWidth()
+                .padding(dimensionResource(R.dimen.padding_medium))
+        )
+    }
+}
