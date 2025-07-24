66e1fd2052414381f41717f0135997a7d28f69d2
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-24 11:36:51 +0900

    feat: android

--- app/src/main/java/com/masahikokobayashi/androidlab/ui/GameScreen.kt
+++ app/src/main/java/com/masahikokobayashi/androidlab/ui/GameScreen.kt
@@ -1,12 +1,16 @@
 package com.masahikokobayashi.androidlab.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -19,13 +23,21 @@ fun GameScreen(
     gameViewModel: GameViewModel = viewModel(),
 ) {
     val gameUiState by gameViewModel.uiState.collectAsState()
+    val mediumPadding = dimensionResource(R.dimen.padding_medium)
 
     Column(modifier = modifier) {
         Text(
             text = stringResource(R.string.app_name),
             style = typography.titleLarge
         )
-        GameLayout()
+        GameLayout(
+            currentScrambledWord = gameUiState.currentScrambledWord,
+            onUserGuessChanged = { gameViewModel.updateUserGuess(it) },
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .padding(mediumPadding)
+        )
     }
 }
 
