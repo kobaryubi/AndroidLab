0c0b4660e2ae20c15923ced993dee78a17ba490e
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-24 11:46:30 +0900

    feat: android

--- app/src/main/java/com/masahikokobayashi/androidlab/ui/GameUiState.kt
+++ app/src/main/java/com/masahikokobayashi/androidlab/ui/GameUiState.kt
@@ -1,5 +1,6 @@
 package com.masahikokobayashi.androidlab.ui
 
 data class GameUiState(
-    val currentScrambledWord: String = ""
+    val currentScrambledWord: String = "",
+    val isGuessedWordWrong: Boolean = false,
 )
