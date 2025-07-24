8ad525e38f6408ae2a89b5ed5ddaa3d3a68123dc
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-24 11:04:50 +0900

    feat: android

--- app/src/main/java/com/masahikokobayashi/androidlab/ui/GameLayout.kt
+++ app/src/main/java/com/masahikokobayashi/androidlab/ui/GameLayout.kt
@@ -1,9 +1,14 @@
 package com.masahikokobayashi.androidlab.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.MaterialTheme.shapes
 import androidx.compose.material3.MaterialTheme.typography
@@ -11,11 +16,15 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.masahikokobayashi.androidlab.R
 import com.masahikokobayashi.androidlab.ui.theme.UnscrambleTheme
 
@@ -24,18 +33,29 @@ fun GameLayout(modifier: Modifier = Modifier) {
     val mediumPadding = dimensionResource(R.dimen.padding_medium)
 
     Card(
+        elevation = CardDefaults.cardElevation(defaultElevation = 5.dp),
         modifier = modifier
     ) {
         Column(
+            verticalArrangement = Arrangement.spacedBy(mediumPadding),
+            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.padding(mediumPadding)
         ) {
+            Text(
+                text = stringResource(R.string.word_count, 0),
+                style = typography.titleMedium,
+                color = colorScheme.onPrimary,
+                modifier = Modifier.clip(shapes.medium)
+                    .background(colorScheme.surfaceTint)
+                    .padding(horizontal = 10.dp, vertical = 4.dp)
+                    .align(alignment = Alignment.End)
+            )
             Text(
                 text = "scrambleun",
                 style = typography.displayMedium
             )
             Text(
                 text = stringResource(R.string.instructions),
-                textAlign = TextAlign.Center,
                 style = typography.titleMedium,
             )
             OutlinedTextField(
@@ -54,6 +74,12 @@ fun GameLayout(modifier: Modifier = Modifier) {
                     focusedContainerColor = colorScheme.surface,
                     unfocusedContainerColor = colorScheme.surface,
                     disabledContainerColor = colorScheme.surface,
+                ),
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = {}
                 )
             )
         }
