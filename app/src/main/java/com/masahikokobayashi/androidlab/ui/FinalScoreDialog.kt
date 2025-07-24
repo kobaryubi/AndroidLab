8da950ec6004dee26e65d6b9d7e9d39694c943c0
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-24 16:07:28 +0900

    feat: android

---
+++ app/src/main/java/com/masahikokobayashi/androidlab/ui/FinalScoreDialog.kt
@@ -0,0 +1,43 @@
+package com.masahikokobayashi.androidlab.ui
+
+import android.app.Activity
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import com.masahikokobayashi.androidlab.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FinalScoreDialog(
+    score: Int,
+    onPlayAgain: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val activity = (LocalContext.current as Activity)
+
+    AlertDialog(
+        onDismissRequest = {},
+        title = { Text(text = stringResource(R.string.congratulations)) },
+        text = { Text(text = stringResource(R.string.you_scored, score)) },
+        dismissButton = {
+            TextButton(
+                onClick = {
+                    activity.finish()
+                }
+            ) {
+                Text(text = stringResource(R.string.exit))
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onPlayAgain) {
+                Text(text = stringResource(R.string.play_again))
+            }
+        },
+        modifier = modifier
+    )
+}
