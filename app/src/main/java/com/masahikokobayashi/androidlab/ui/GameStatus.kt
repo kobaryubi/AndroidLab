2f5b6a8b37a7e2f53c83793cc50bec1906a889a2
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-24 13:05:23 +0900

    feat: android

---
+++ app/src/main/java/com/masahikokobayashi/androidlab/ui/GameStatus.kt
@@ -0,0 +1,22 @@
+package com.masahikokobayashi.androidlab.ui
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme.typography
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.masahikokobayashi.androidlab.R
+
+@Composable
+fun GameStatus(score: Int, modifier: Modifier = Modifier) {
+    Card(modifier = modifier) {
+        Text(
+            text = stringResource(R.string.score, score),
+            style = typography.headlineMedium,
+            modifier = Modifier.padding(8.dp),
+        )
+    }
+}
