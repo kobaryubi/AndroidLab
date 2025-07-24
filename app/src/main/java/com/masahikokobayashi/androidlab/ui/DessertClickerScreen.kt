5f0192f76ab2645f71d6c03d91c3afb852270097
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-24 19:25:30 +0900

    feat: android

---
+++ app/src/main/java/com/masahikokobayashi/androidlab/ui/DessertClickerScreen.kt
@@ -0,0 +1,13 @@
+package com.masahikokobayashi.androidlab.ui
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun DessertClickerScreen(
+    modifier: Modifier = Modifier
+) {
+    Box(modifier = modifier) {
+    }
+}
