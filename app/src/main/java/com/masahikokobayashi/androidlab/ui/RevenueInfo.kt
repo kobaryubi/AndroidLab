3414e62eb52ee54f6024490894cd5659f178e35b
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-25 05:32:03 +0900

    feat: android

---
+++ app/src/main/java/com/masahikokobayashi/androidlab/ui/RevenueInfo.kt
@@ -0,0 +1,34 @@
+package com.masahikokobayashi.androidlab.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import com.masahikokobayashi.androidlab.R
+
+@Composable
+fun RevenueInfo(
+    revenue: Int,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = modifier,
+    ) {
+        Text(
+            text = stringResource(R.string.total_revenue),
+            style = MaterialTheme.typography.headlineMedium,
+            color = MaterialTheme.colorScheme.onSecondaryContainer
+        )
+        Text(
+            text = "$${revenue}",
+            textAlign = TextAlign.Right,
+            style = MaterialTheme.typography.headlineMedium,
+            color = MaterialTheme.colorScheme.onSecondaryContainer
+        )
+    }
+}
