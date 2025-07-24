d29b590a1479b3c00c3feabdc29d7388d68ae065
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-24 11:58:13 +0900

    feat: android

--- app/src/main/java/com/masahikokobayashi/androidlab/ui/GameViewModel.kt
+++ app/src/main/java/com/masahikokobayashi/androidlab/ui/GameViewModel.kt
@@ -8,6 +8,7 @@ import com.masahikokobayashi.androidlab.data.allWords
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class GameViewModel : ViewModel() {
     private val _uiState = MutableStateFlow(GameUiState())
@@ -55,7 +56,9 @@ class GameViewModel : ViewModel() {
         if (userGuess.equals(currentWord, ignoreCase = true)) {
 
         } else {
-
+            _uiState.update { currentState ->
+                currentState.copy(isGuessedWordWrong = true)
+            }
         }
 
         updateUserGuess("")
