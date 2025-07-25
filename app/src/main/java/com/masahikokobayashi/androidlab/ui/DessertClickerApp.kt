ea6fd616bccd19aa8381a8080615b944bd0e16e1
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-25 11:00:07 +0900

    feat: android

--- app/src/main/java/com/masahikokobayashi/androidlab/ui/DessertClickerApp.kt
+++ app/src/main/java/com/masahikokobayashi/androidlab/ui/DessertClickerApp.kt
@@ -1,6 +1,5 @@
 package com.masahikokobayashi.androidlab.ui
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
@@ -14,22 +13,20 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLayoutDirection
 import com.masahikokobayashi.androidlab.model.Dessert
-
-private const val TAG = "DessertClickerApp"
 
 @Composable
 fun DessertClickerApp(
     desserts: List<Dessert>,
     modifier: Modifier = Modifier
 ) {
-    var revenue by remember { mutableIntStateOf(0) }
-    var dessertsSold by remember { mutableIntStateOf(0) }
-    var currentDessertIndex by remember { mutableIntStateOf(0) }
+    var revenue by rememberSaveable { mutableIntStateOf(0) }
+    var dessertsSold by rememberSaveable { mutableIntStateOf(0) }
+    var currentDessertIndex by rememberSaveable { mutableIntStateOf(0) }
     val currentDessertImageId = desserts[currentDessertIndex].imageId
     val currentDessertPrice = desserts[currentDessertIndex].price
 
