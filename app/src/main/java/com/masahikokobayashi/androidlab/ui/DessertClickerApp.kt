f12458944101255b7de1ce6092e07909b163c41d
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-25 10:53:36 +0900

    feat: android

--- app/src/main/java/com/masahikokobayashi/androidlab/ui/DessertClickerApp.kt
+++ app/src/main/java/com/masahikokobayashi/androidlab/ui/DessertClickerApp.kt
@@ -1,6 +1,15 @@
 package com.masahikokobayashi.androidlab.ui
 
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -8,7 +17,10 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLayoutDirection
 import com.masahikokobayashi.androidlab.model.Dessert
+
+private const val TAG = "DessertClickerApp"
 
 @Composable
 fun DessertClickerApp(
@@ -22,7 +34,22 @@ fun DessertClickerApp(
     val currentDessertPrice = desserts[currentDessertIndex].price
 
     Scaffold(
-        topBar = {},
+        topBar = {
+            val layoutDirection = LocalLayoutDirection.current
+
+            DessertClickerAppBar(
+                onShareButtonClicked = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        start = WindowInsets.safeDrawing.asPaddingValues()
+                            .calculateStartPadding(layoutDirection),
+                        end = WindowInsets.safeDrawing.asPaddingValues()
+                            .calculateEndPadding(layoutDirection),
+                    )
+                    .background(MaterialTheme.colorScheme.primary)
+            )
+        },
         modifier = modifier,
     ) { contentPadding ->
         DessertClickerScreen(
