913e1352872dc0b1e1d6fac393406a37184e9c77
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-11-09 10:52:33 +0900

    feat: lab

--- app/src/main/java/com/masahikokobayashi/androidlab/MainActivity.kt
+++ app/src/main/java/com/masahikokobayashi/androidlab/MainActivity.kt
@@ -8,13 +8,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawingPadding
-import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -28,9 +23,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -41,10 +34,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.rememberNavController
-import com.masahikokobayashi.androidlab.ui.GameScreen
 import com.masahikokobayashi.androidlab.ui.theme.AndroidLabTheme
 import com.masahikokobayashi.androidlab.ui.theme.LemonadeTheme
-import com.masahikokobayashi.androidlab.ui.theme.UnscrambleTheme
 
 private const val TAG = "MainActivity"
 
@@ -253,24 +244,4 @@ fun SearchBarPreview() {
     AndroidLabTheme {
         SearchBar()
     }
-}
-
-@Composable
-fun BusinessCard(modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier
-    ) {
-        Column {}
-        Column {
-            Row {}
-            Row {}
-            Row {}
-        }
-    }
-}
-
-@Preview
-@Composable
-fun BusinessCardPreview() {
-    BusinessCard()
 }
